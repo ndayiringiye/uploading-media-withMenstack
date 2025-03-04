726849f5f9871c303b5ba1cd1977633ada303116
ndayingiye <ndayiringiyedavid120@gmail.com>
2025-03-04 16:25:37 +0200

    chore/frontend setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,10 +2,12 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDb } from "./config/db.js";
 import userRouter from "./routes/userRoute.js"
+import cors from "cors"
 dotenv.config();
 const app = express();
 app.use(express.json());
 app.use("/api/user", userRouter);
+app.use(cors())
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, async () => {
